fd2f370017b83f03a08c0259b80b718ecc561e60
kenziner <nicouebeglahkenza@gmail.com>
2024-05-27 15:20:42 +0000

    Ajoutez des fichiers projet.

---
+++ GestionConference/AppContext/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using GestionConference.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionConference.AppContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        DbSet<User> Users { get; set; }
+    }
+}
---
+++ GestionConference/Program.cs
@@ -0,0 +1,29 @@
+using GestionConference.AppContext;
+using GestionConference.Components;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var connectionString = builder.Configuration.GetConnectionString("GestionConferenceConnexion");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
